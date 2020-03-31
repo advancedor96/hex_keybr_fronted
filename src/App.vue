dcf9a4e9169bf3837c48a10efd4b1858bfa41b7a
丁丁 <dingding@bxb.tw>
2020-03-31 17:45:45 +0800

    第一版

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+html, body{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" ref="aaa">
+home
+  <canvas ref="myChart" width="400" height="400"></canvas>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Chart from 'chart.js'
+import axios from 'axios'
+function getRandomColor () {
+  var letters = '0123456789ABCDEF'
+  var color = '#'
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data: () => ({
+    myChart: null,
+    userList: null
+  }),
+  mounted () {
+    this.getData()
+    this.chart = this.$refs.myChart
+  },
+  methods: {
+    makeChart () {
+      // to do: 抓出幾天 moment.js
+      window.user = this.userList
+      const mydataset = this.userList.map((e, i) => ({
+        label: e.nickName,
+        data: e.grade,
+        backgroundColor: getRandomColor(),
+        borderColor: getRandomColor(),
+        fill: false
+
+      }))
+      var myChart = new Chart(this.chart, {
+        type: 'line',
+        data: {
+          labels: Array.from(Array(21), (e, i) => i + 1),
+          datasets: mydataset
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          }
+        }
+      })
+    },
+    async getData () {
+      try {
+        const res = await axios.get('http://localhost:3000/data')
+        this.userList = res.data
+        console.log('user', this.userList)
+        this.makeChart()
+        const days = 21
+      } catch (err) {
+        console.log('err', err)
+      }
+      console.log('get data')
+    }
   }
 }
 </script>
